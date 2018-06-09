5a4b229b2c939958ef905207dcf8a07d337c3b9b
melgarejojunior <melgarejojunior@gmail.com>
2018-06-09 16:13:16 -0400

    Finished some details with leds and db constants

--- app/src/main/java/com/example/melgarejo/youaretooloud/HomeActivity.kt
+++ app/src/main/java/com/example/melgarejo/youaretooloud/HomeActivity.kt
@@ -10,18 +10,34 @@ import android.os.Build
 import android.os.Bundle
 import android.os.Handler
 import android.util.Log
+import com.google.android.things.contrib.driver.button.ButtonInputDriver
+import com.google.android.things.pio.Gpio
+import com.google.android.things.pio.PeripheralManager
 import com.google.firebase.database.*
 
 
 class HomeActivity : Activity(), MediaPlayer.OnPreparedListener {
 
+    companion object {
+        private const val TAG = "HomeActivity"
+        private const val DB_ROOT_NAME = "android_things"
+        private const val DB_DELAY = "delay"
+        private const val DB_PLAY_IT = "sound"
+    }
+
+
     private lateinit var mediaPlayer: MediaPlayer
     private lateinit var database: FirebaseDatabase
     private lateinit var databaseReference: DatabaseReference
+    private lateinit var ledRedGpio: Gpio
+    private lateinit var ledGreenGpio: Gpio
+    private lateinit var ledBlueGpio: Gpio
+    private lateinit var buttonInputDriver: ButtonInputDriver
+
     private var mListener: ValueEventListener
 
-    private val TAG = "HomeActivity"
-    private val DELAY_IN_MILISECONDS: Long = 5000
+    private var delayInMilliSeconds = 1300L
+    private var initialized = false
 
     init {
         mListener = object : ValueEventListener {
@@ -30,41 +46,29 @@ class HomeActivity : Activity(), MediaPlayer.OnPreparedListener {
             }
 
             override fun onDataChange(snapshot: DataSnapshot) {
-                val value = snapshot.child("sound").getValue(Boolean::class.java) ?: false
-                if (value && !mediaPlayer.isPlaying) {
-                    mediaPlayer.prepareAsync()
-
+                delayInMilliSeconds = snapshot.child(DB_DELAY).getValue(Long::class.java) ?: delayInMilliSeconds
+                val shouldPlayIt = snapshot.child(DB_PLAY_IT).getValue(Boolean::class.java) ?: false
+                if (shouldPlayIt && !mediaPlayer.isPlaying) {
+                    try {
+                        mediaPlayer.prepareAsync()
+                    } catch (t: Throwable) {
+                        playSong(mediaPlayer)
+                    }
                 } else {
                     if (mediaPlayer.isPlaying) mediaPlayer.stop()
                 }
-                Log.d(TAG, value.toString())
+                Log.d(TAG, shouldPlayIt.toString())
             }
         }
-    }
-
-    private fun startTimer(mp: MediaPlayer?) {
-        val handler = Handler()
-        handler.postDelayed({
-            mp?.stop()
-            databaseReference.child("sound").setValue(false)
-        }, DELAY_IN_MILISECONDS)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
-        Log.e(TAG, "ANTES DE ABRIR O MEDIA PLAYER")
-        mediaPlayer = MediaPlayer.create(this, R.raw.siren)
-        mediaPlayer.isLooping = true
-        mediaPlayer.setOnPreparedListener(this)
-        while(!isConnectedToWifi(this)) {
-            Log.e(TAG, "nao está conectado")
-        }
-        Log.e(TAG, "ANTES DE ABRIR O FIREBASE")
-        database = FirebaseDatabase.getInstance()
-        databaseReference = database.getReference("android_things")
-        databaseReference.addValueEventListener(mListener)
-
+        setupInOuts()
+        createMediaPlayerInstance()
+        waitUntilConnected()
+        setupFirebaseDatabase()
     }
 
     override fun onDestroy() {
@@ -73,22 +77,79 @@ class HomeActivity : Activity(), MediaPlayer.OnPreparedListener {
     }
 
     override fun onPrepared(mp: MediaPlayer?) {
+        if (initialized) {
+            playSong(mp)
+        } else {
+            initialized = true
+        }
+    }
+
+    private fun setupInOuts() {
+        val pioService = PeripheralManager.getInstance()
+        ledRedGpio = pioService.openGpio(BoardDefaults.gpioForRedLED)
+        ledRedGpio.setDirection(Gpio.DIRECTION_OUT_INITIALLY_LOW)
+
+        ledGreenGpio = pioService.openGpio(BoardDefaults.gpioForGreenLED)
+        ledGreenGpio.setDirection(Gpio.DIRECTION_OUT_INITIALLY_LOW)
+
+        ledBlueGpio = pioService.openGpio(BoardDefaults.gpioForBlueLED)
+        ledBlueGpio.setDirection(Gpio.DIRECTION_OUT_INITIALLY_LOW)
+    }
+
+    private fun setupFirebaseDatabase() {
+        setLedValue(ledBlueGpio, false)
+        database = FirebaseDatabase.getInstance()
+        databaseReference = database.getReference(DB_ROOT_NAME)
+        databaseReference.addValueEventListener(mListener)
+        databaseReference.child(DB_PLAY_IT).setValue(false)
+        databaseReference.child(DB_DELAY).setValue(delayInMilliSeconds)
+        setLedValue(ledBlueGpio, true)
+    }
+
+    private fun waitUntilConnected() {
+        setLedValue(ledGreenGpio, false)
+        while (!isConnectedToWifi(this)) {
+            Log.e(TAG, "You have to connect to a WiFi network or Ethernet cable")
+        }
+        setLedValue(ledGreenGpio, true)
+    }
+
+    /**
+     * Update the value of the LED output.
+     */
+    private fun setLedValue(led: Gpio, value: Boolean) {
+        Log.d(TAG, "Setting LED ${led.name} value to $value")
+        led.value = value
+    }
+
+    private fun createMediaPlayerInstance() {
+        setLedValue(ledRedGpio, false)
+        mediaPlayer = MediaPlayer.create(this, R.raw.bell)
+        mediaPlayer.isLooping = true
+        mediaPlayer.setOnPreparedListener(this)
+        setLedValue(ledRedGpio, true)
+    }
+
+    private fun playSong(mp: MediaPlayer?) {
         mp?.start()
         startTimer(mp)
     }
 
-    fun isConnectedToWifi(context: Context): Boolean {
-        val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-        return if (Build.VERSION.SDK_INT > 23) {
-            isConnectedToWifiAfterMarshmallow(connectivityManager)
-        } else {
-            isConnectedToWifiBeforeMarshmallow(connectivityManager)
-        }
+    private fun startTimer(mp: MediaPlayer?) {
+        val handler = Handler()
+        handler.postDelayed({
+            try {
+                mp?.stop()
+                databaseReference.child(DB_PLAY_IT).setValue(false)
+            } catch (t: Throwable) {
+                Log.e(TAG, t.cause?.message)
+            }
+        }, delayInMilliSeconds)
     }
 
-    private fun isConnectedToWifiBeforeMarshmallow(connectivityManager: ConnectivityManager): Boolean {
-        val networkInfo = connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI)
-        return networkInfo != null && networkInfo.isConnected
+    private fun isConnectedToWifi(context: Context): Boolean {
+        val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        return isConnectedToWifiAfterMarshmallow(connectivityManager)
     }
 
     @TargetApi(Build.VERSION_CODES.M)
